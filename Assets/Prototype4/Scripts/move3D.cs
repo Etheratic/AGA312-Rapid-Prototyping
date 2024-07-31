04bcb170331e6bba299e47b7c22201a78ed6bcb4
Etheratic <136394417+Etheratic@users.noreply.github.com>
2024-07-31 13:08:46 +1000

    prototype4, build

--- Assets/Prototype4/Scripts/move3D.cs
+++ Assets/Prototype4/Scripts/move3D.cs
@@ -9,6 +9,7 @@ public class move3D : GameBehaviour
     private Camera mainCamera;
     private float CameraZDistance;
     public float width;
+    public bool isSelected;
       
 
     // Start is called before the first frame update
@@ -16,22 +17,46 @@ public class move3D : GameBehaviour
     {
         mainCamera = Camera.main;
         CameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
-        width = gameObject.transform.localScale.x;
+       
+     
     }
 
    
     private void OnMouseDrag()
     {
+        isSelected = true;
         Vector3 ScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, CameraZDistance);
         Vector3 NewWorldPos = mainCamera.ScreenToWorldPoint(ScreenPos);
         transform.position = NewWorldPos;
     }
 
-
+    private void OnMouseUp()
+    {
+        isSelected = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
+
         
+
+        if (Input.GetKey(KeyCode.Alpha1) && isSelected == true)
+        {
+            width += 10;
+            
+
+        }
+
+        if (Input.GetKey(KeyCode.Alpha2) && isSelected == true)
+        {
+
+            width -= 10;
+            width = gameObject.GetComponent<Transform>().localScale.x;
+
+
+        }
+
+        width = gameObject.GetComponent<Transform>().localScale.x;
     }
 }
