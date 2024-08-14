3219891ee0409c0057b81904ea48da4f940c8dd1
Etheratic <136394417+Etheratic@users.noreply.github.com>
2024-08-14 18:58:12 +1000

    Day/Night Cycle

---
+++ Assets/Prototype5/Scripts/LightingManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+
+public class LightingManager : MonoBehaviour
+{
+    //references
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightingPreset preset;
+
+    //Variables
+    [SerializeField, Range(0, 24)] private float timeOfDay;
+    public int dayCounter;
+    private bool dayCounted = false;
+    public float timeSpeed = 1;
+
+    private void Update()
+    {
+        if (preset == null)
+            return;
+
+        if(Application.isPlaying)
+        {
+            timeOfDay += Time.deltaTime * timeSpeed;
+            timeOfDay %= 24; //clamp between 0 - 24
+            UpdateLighting(timeOfDay / 24f);
+
+            if (timeOfDay >= 23)
+            {
+                dayCounter += 1;
+                
+                
+                timeOfDay = 0;
+            }
+        }
+        //else
+        //{
+        //    UpdateLighting(timeOfDay / 24f);
+        //}
+
+       
+    }
+
+    //private void DayCounted()
+    //{
+    //    dayCounted = false;
+    //}
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = preset.ambientColour.Evaluate(timePercent);
+        RenderSettings.fogColor = preset.fogColour.Evaluate(timePercent);
+
+        if (directionalLight != null)
+        {
+            directionalLight.color = preset.directionalColour.Evaluate(timePercent);
+
+            directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0)); 
+        }
+    }
+
+
+    private void OnValidate()
+    {
+        if (directionalLight != null)
+            return;
+
+        if(RenderSettings.sun != null)
+        {
+            directionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if(light.type == LightType.Directional)
+                {
+                    directionalLight = light;
+                    return;
+                }
+            }
+
+        }
+
+        
+    }
+
+
+}
--- Assets/Prototype5/Scripts/SpriteBillboard.cs
+++ Assets/Prototype5/Scripts/SpriteBillboard.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
+    [SerializeField] bool freezeXZAxis = true;
   
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        if(freezeXZAxis)
+        {
+            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        }
+        else
+        {
+            transform.rotation = Camera.main.transform.rotation;
+        }
+                
     }
 }
